a52edb28e8114e73ed19f47d3d1438cb440692eb
jules vannuijs <jules.vannuijs@bee-organisation.com>
2024-03-02 20:56:35 +0100

    architectuur aangepast

--- CypressZandbak/cypress/tests/Klei/Basis 1 - Cypress wat is dat.cy.js
+++ CypressZandbak/cypress/tests/Klei/Basis 1 - Cypress wat is dat.cy.js
@@ -11,6 +11,7 @@ describe("Cypress is een framework dat JavaScript gebruikt om websites te testen
     })
 
     it("Oefenen met het commando cy.log", () => {
+        cy.pause()
         cy.log('het linkermenu noemen we het commandlog of gewoon log')
         cy.wacht()
         cy.log('log: logboek van de test')
